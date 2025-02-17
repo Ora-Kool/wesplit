051ae4286e9f1f8a2e5c6dc3b35f0b0acbe6d4e1
Etinge Mabian <etingemabian@gmail.com>
2025-02-17 14:05:26 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by TheLyte on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
